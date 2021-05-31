3449901e8f1b22473f7c6cb62de886fa3ab57fbf
Dong Jin <ilovethisid@naver.com>
2021-05-31 21:57:53 +0900

    Merge branch 'Subgroup_1' of https://github.com/EatChangmyeong/software-engineering into Subgroup_1
    change picture

--- Implementation using Django & Python/doctor_patient_portal/catalog/urls.py
+++ Implementation using Django & Python/doctor_patient_portal/catalog/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('login_request/', views.login_request, name='login_request'),
     path('register/', views.register, name='register'),
     path('register_request/', views.register_request, name='register_request'),
+    path('user/', views.user_page, name='user_page'),
+    path('user_edit/', views.user_edit, name='user_edit'),
+    path('user_edit_request/', views.user_edit_request, name='user_edit_request'),
     path('logout/', views.logout, name='logout'),
 ]
